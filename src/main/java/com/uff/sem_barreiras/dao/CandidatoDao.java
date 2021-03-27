10089bf5e18a68bafe14b7e8cae51af4de8faeb2
Victor Marques <victyrmarques@gmail.com>
2021-03-27 17:55:44 -0300

    Criação dos dao, service e controller faltantes

--- src/main/java/com/uff/sem_barreiras/dao/CandidatoDao.java
+++ src/main/java/com/uff/sem_barreiras/dao/CandidatoDao.java
@@ -1,11 +1,11 @@
 package com.uff.sem_barreiras.dao;
 
-import com.uff.sem_barreiras.model.Vaga;
+import com.uff.sem_barreiras.model.Candidato;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VagaDao extends  JpaRepository<Vaga,Integer> {
+public interface CandidatoDao extends  JpaRepository<Candidato,Integer> {
     
 }
