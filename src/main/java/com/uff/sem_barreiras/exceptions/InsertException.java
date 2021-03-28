0a418a99abd7ec2bc877f28eadd086222fae6841
Victor Marques <victyrmarques@gmail.com>
2021-03-27 21:38:58 -0300

    Ajuste local de exceção para EstadoController

--- src/main/java/com/uff/sem_barreiras/exceptions/InsertException.java
+++ src/main/java/com/uff/sem_barreiras/exceptions/InsertException.java
@@ -1,7 +1,9 @@
 package com.uff.sem_barreiras.exceptions;
 
 public class InsertException extends Exception{
-    
+
+    private static final long serialVersionUID = 1L;
+
     public InsertException(String campo, String objeto){
         super( "Ausência do campo '" + campo + "' para realizar a criação d" + objeto);
     }
--- src/main/java/com/uff/sem_barreiras/exceptions/NotFoundException.java
+++ src/main/java/com/uff/sem_barreiras/exceptions/NotFoundException.java
@@ -2,6 +2,8 @@ package com.uff.sem_barreiras.exceptions;
 
 public class NotFoundException extends Exception{
 
+    private static final long serialVersionUID = 1L;
+
     public NotFoundException(String nomeObjeto, Integer id){
         super( nomeObjeto + " com id = " + id +" não encontrado ");
     }
