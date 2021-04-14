a02b61d35510ee3d3fee2d09fc359055f79a1796
Victor Marques <victyrmarques@gmail.com>
2021-04-14 20:25:22 -0300

    Alterações para abrir index com a iniciação do Spring

--- src/main/java/com/uff/sem_barreiras/SemBarreirasApplication.java
+++ src/main/java/com/uff/sem_barreiras/SemBarreirasApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
@@ -31,4 +33,21 @@ public class SemBarreirasApplication extends WebMvcConfigurationSupport{
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**");
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**")
+			.addResourceLocations("classpath:/static/img/");
+		registry.addResourceHandler("/scripts/**")
+			.addResourceLocations("classpath:/static/scripts/");
+		registry.addResourceHandler("/css/**")
+			.addResourceLocations("classpath:/static/css/");
+	}
+
+	//mapeamento dos HTML do front devem ser colocados aqui
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+	
 }
