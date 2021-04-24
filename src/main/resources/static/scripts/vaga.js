46583c49a88debebfd6481c4a7ac0b18c25d9d15
Breno Froes <breno_froes@id.uff.br>
2021-04-23 22:56:13 -0300

    Criacao da pagina acessar vaga

--- src/main/resources/static/scripts/vaga.js
+++ src/main/resources/static/scripts/vaga.js
@@ -1,5 +1,91 @@
 $(document).ready( function(){
-    $.getJSON("/vaga/", {}, function(data) { 
-        
-     });
+    $.getJSON("/vaga", {}, function(data) { 
+
+        for(var i =0; i < data.totalElements; i++){
+            var vaga = data.content[i];
+    
+            var body = document.getElementById('corpo');
+            var div = document.createElement('div'); //create a div
+            div.innerHTML = createMyElement(vaga);   
+            div.id = vaga.id;                      //add an id
+            body.appendChild(div);                 //append to the doc.body
+            body.insertBefore(div, body.firstChild) //OR insert it
+        }
+    }); 
+
 });
+
+function createMyElement(vaga){
+    var local = vaga.empresa.cidade.nome + ' - ' + vaga.empresa.cidade.estado.uf;
+    return [
+        '<div class="row">', 
+            '<div class="col-2">',
+                '<img src="../img/Rectangle.png" class="imgVaga">',
+            '</div>',
+            '<div class="col-9">',
+                '<h3 class="tituloVaga">', vaga.resumo,'</h3>',
+                '<p class="textoLocalVaga">', local,'</p>',
+                '<p class="textoEmpresaVaga">', vaga.empresa.nome,'</p>',
+                '<p class="textoEmpresaVaga">', vaga.nivel,'</p>',
+                '<p class="textoEmpresaVaga">', vaga.area.descricao,'</p>',
+            '</div>',
+            '<div class="col-1">',
+                '<img src="../img/Vector2.png" class="vectorVaga1">',
+                '<br>',
+                '<img src="../img/Vector.png" class="vectorVaga2 mt-4">',
+            '</div>',
+        '</div>',
+        `<div class="row my-3">`,
+            '<div class="col-12">',
+                '<h2 class="tituloVaga"> Descrição da vaga: </h2>',
+                '<p class="textoLocalVaga">', vaga.resumo,'</p>',
+            '</div>',
+        '</div>', 
+        `<div class="row">`,
+            '<div class="col-12">',
+                '<h2 class="tituloVaga"> Requisitos necessários: </h2>',
+                '<ul>',
+                    '<li>',vaga.requisitosNecessarios,'</li>',
+                    '<li>Domínio em PHP, CSS, Java Script, React, Bancos de dados</li>',
+                '</ul>',
+            '</div>',
+        '</div>', 
+        `<div class="row">`,
+            '<div class="col-12">',
+                '<h2 class="tituloVaga"> Requisitos desejados: </h2>',
+                '<ul>',
+                    '<li>',vaga.requisitosDesejados,'</li>',
+                    '<li>Domínio em PHP, CSS, Java Script, React, Bancos de dados</li>',
+                '</ul>',
+            '</div>',
+        '</div>',
+        `<div class="row">`,
+            '<div class="col-6">',
+                '<h2 class="tituloVaga"> Escolaridade: </h2>',
+                '<ul>',
+                    '<li>',vaga.escolaridade.descricao,'</li>',
+                '</ul>',
+            '</div>',
+            '<div class="col-6">',
+                '<h2 class="tituloVaga"> Jornada de Trabalho: </h2>',
+                '<ul>',
+                    '<li>',vaga.jornadaTrabalho,'</li>',
+                '</ul>',
+            '</div>',
+        '</div>', 
+        `<div class="row">`,
+            '<div class="col-6">',
+                '<h2 class="tituloVaga"> Benefícios: </h2>',
+                '<ul>',
+                    '<li>',vaga.beneficios,'</li>',
+                '</ul>',
+            '</div>',
+            '<div class="col-6">',
+                '<h2 class="tituloVaga"> Faixa salarial: </h2>',
+                '<ul>',
+                    '<li>',vaga.remuneracao,'</li>',
+                '</ul>',
+            '</div>',
+        '</div>', 
+        ].join('\n');
+ }
